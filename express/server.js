15c36cc81cb93232dd1828ecc44827966e6d0e54
its-ankur <ankuratul2007@gmail.com>
2023-08-26 10:48:00 +0530

    get and post method

--- express/server.js
+++ express/server.js
@@ -3,9 +3,21 @@ const path = require("path");
 //variable,function,object,class
 //express object 1 default function
 
-const app=express();
+const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false })); //for post data
+app.get("/getdata", (req, res) => {
+    //res.send("Welcome to getdata page ...");
+    console.log(req.query);
+    //res.send(req.query);
+    res.send("Welcome " + req.query.username);
+});
+
+app.post("/postdata", (req, res) => {
+    console.log(req.body);
+    res.send("Welcome " + req.body.username);
+});
 
 
 // app.get("/",(req,res)=>{    //request,response
@@ -15,9 +27,9 @@ app.use(express.static("public"));
 //     //res.send("Welcome to index page ...");
 //     res.sendFile(path.join(__dirname,"index.html"));
 // });
-app.get("*",(req,res)=>{
-    res.status(404).sendFile(path.join(__dirname,"public/404.html"));
+app.get("*", (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "public/404.html"));
 });
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server is running on port 3000");
 });
