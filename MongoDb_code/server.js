f6cd51e87d7546b43f4e7ad4bde12bce2799ecb5
its-ankur <ankuratul2007@gmail.com>
2023-10-06 15:07:59 +0530

    mongo start

--- MongoDb_code/server.js
+++ MongoDb_code/server.js
@@ -1,7 +1,13 @@
 const express=require("express");
 const app=express();
 const client=require("mongodb").MongoClient;
-
+let dbinstance;
+client.connect("mongodb://127.0.0.1:27017").then((server)=>{
+    dbinstance=server.db("globe");
+console.log("Connected to database");
+}).catch((error)=>{
+    console.log(error);
+});
 
 app.listen(3000,()=>{
     console.log("Server started at port 3000");
