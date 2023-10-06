1b21c115ea90ef68128e35e7cc122d387d34ced3
its-ankur <ankuratul2007@gmail.com>
2023-10-06 14:28:57 +0530

    mongodb

---
+++ CQ St-1 questions/Question 2/server.js
@@ -0,0 +1,6 @@
+const fs=require('fs');
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    
+});
---
+++ MongoDb_code/server.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const app=express();
+const client=require("mongodb").MongoClient;
+
+
+app.listen(3000,()=>{
+    console.log("Server started at port 3000");
+});
