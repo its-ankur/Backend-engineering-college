e1469a648caf2f41b4b24d974c7542d7d358fbb4
its-ankur <ankuratul2007@gmail.com>
2023-10-10 10:46:06 +0530

    MongoDb day2

--- MongoDb_code/server.js
+++ MongoDb_code/server.js
@@ -1,13 +1,32 @@
 const express=require("express");
 const app=express();
 const client=require("mongodb").MongoClient;
+app.set("view engine","ejs");
 let dbinstance;
 client.connect("mongodb://127.0.0.1:27017").then((server)=>{
-    dbinstance=server.db("globe");
+    dbinstance=server.db("Globe");
 console.log("Connected to database");
 }).catch((error)=>{
     console.log(error);
 });
+
+app.get("/getData",(req,res)=>{
+    dbinstance.collection("students").find({}).toArray().then((data)=>{
+        // console.log(data);
+        // res.end();
+        res.render("home",{students:data});
+    });
+});
+
+app.get("/saveData",(req,res)=>{
+    let obj={};
+    obj.name=req.query.name;
+    obj.age=req.query.age;
+    dbinstance.collection("students").insertOne(obj).then((response)=>{
+        res.redirect("/getData");
+    });
+});
+
 
 app.listen(3000,()=>{
     console.log("Server started at port 3000");
