4031cf55c9e683d1b12734fb3710a800ae94275e
its-ankur <ankuratul2007@gmail.com>
2023-10-27 15:14:40 +0530

    Something

--- Multer/server.js
+++ Multer/server.js
@@ -1,32 +1,32 @@
 const express = require("express");
-const multer = require("multer");
 const app = express();
 const path = require("path");
-const fs = require("fs");
-app.use(express.static("public"));
+const multer = require("multer");
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "public/files");
+    destination: function (req, file, cb) {
+        cb(null, 'public/files');
     },
-    filename: (req, file, cb) => {
-        let ext = file.minetype.split("/")[1];
+    filename: function (req, file, cb) {
+        console.log(file);
+        let ext = file.mimetype.split("/")[1];
         cb(null, req.session.username + "." + ext);
     }
 });
 function test(req, file, cb) {
-    let ext = file.minetype.split("/")[1];
+    let ext = file.mimetype.split("/")[1];
     if (ext == "jpeg" || ext == "jpg")
         cb(null, true);
     else
-        cb(new Error("File type is not supported"), false);
+        cb(new Error('FIle not supported'), false);
 }
-const upload = multer({ storage: storage, fileFilter: test,limits:{fileSize:1000000} });
+const upload = multer({ storage: storage, fileFilter: test, limits: { fileSize: 1020 } });
+app.use(express.static("public"));
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/form.html"));
-});
+    res.sendFile(path.join(__dirname, "/public/forms.html"));
+})
 app.post("/upload", upload.single("pic"), (req, res) => {
-    res.end("File uploaded");
-});
-app.listen(3000, () => {
-    console.log("Server started");
-});
+    res.end();
+})
+app.listen(3000, (err) => {
+    console.log("Server Started...");
+})
