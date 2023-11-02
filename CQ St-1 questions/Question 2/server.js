0239a2f632d1c2b93ac2bfacc62afc50ff499625
its-ankur <ankuratul2007@gmail.com>
2023-11-02 17:34:41 +0530

    Question 2

--- CQ St-1 questions/Question 2/server.js
+++ CQ St-1 questions/Question 2/server.js
@@ -1,6 +1,37 @@
 const fs=require('fs');
 const express=require('express');
 const app=express();
+//app.use(express.static('public'));
 app.get('/',(req,res)=>{
-    
+    res.send('Hello World');
+});
+app.get('/products',(req,res)=>{
+    if(req.query.category){
+        fs.readFile('./public/products.json',(err,data)=>{
+            if(err){
+                res.status(500).send('Something went wrong');
+            }
+            let products=JSON.parse(data);
+            let filterProduct=products.filter((product)=>product.category===req.query.category);
+            res.send(filterProduct);
+           //console.log(filterProduct);
+        });
+    }
+    //res.send("Please specify product to find");
+});
+app.get('/filterproducts',(req,res)=>{
+    if(req.query.category){
+        fs.readFile('./public/products.json',(err,data)=>{
+            if(err){
+                res.status(500).send('Something went wrong');
+            }
+            let products=JSON.parse(data);
+            let filterProduct=products.filter((product)=>product.category===req.query.category && product.price==req.query.price);
+            res.send(filterProduct);
+        });
+    }
+    //res.send("Please specify product to find");
+});
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000');
 });
